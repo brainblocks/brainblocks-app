3b0f487b3054914d8286704ccaf2a59737fcec8e
Ty Schenk <tyschenk2@me.com>
2019-03-30 02:26:55 -0400

    added security features

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
-// import expressWs from 'express-ws';
+import helmet from 'helmet';
+import csp from 'helmet-csp';
+import bodyParser from 'body-parser';
 
 import router from './routes';
 
 let app = express();
-// let expressWs = expressWs(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +22,45 @@ if (process.env.LOGGING === 'true') {
     app.use(logger('dev'));
 }
 
+// enable helmet
+app.use(helmet());
+
 app.use(cors({
     origin: process.env.WALLET_DOMAIN
 }));
+
+// Sets "Referrer-Policy: same-origin".
+app.use(helmet.referrerPolicy({
+    policy: 'same-origin'
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.WALLET_DOMAIN);
+    next();
+});
+
+// allow CSP violation reports
+app.use(csp({
+    directives: {
+        defaultSrc:              [ "'self'" ], // eslint-disable-line quotes
+        scriptSrc:               [ "'self'", "'unsafe-inline'" ], // eslint-disable-line quotes
+        reportUri:                '/report-violation',
+        upgradeInsecureRequests: true
+    }
+}));
+
+app.use(bodyParser.json({
+    type: [ 'json', 'application/csp-report' ]
+}));
+
+app.post('/report-violation', (req, res) => {
+    if (req.body) {
+        console.log('CSP Violation: ', req.body);
+    } else {
+        console.log('CSP Violation: No data received!');
+    }
+    res.status(204).end();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
